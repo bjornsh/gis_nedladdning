43ccc8f9157c46ef840f286bbcb483604c7df6a1
bjornsh <bsh@posteo.de>
2022-08-18 13:52:47 +0200

    written over with adapted skript

--- lastkajen_api_vagnat_for_transportplanering.R
+++ lastkajen_api_vagnat_for_transportplanering.R
@@ -1,21 +1,74 @@
-library(httr)
-library(jsonlite)
-library(tidyverse)
+##################################################################################
+### Syfte
+##################################################################################
 
-source("G:/skript/func/func_filer.R", encoding = "utf-8", echo = FALSE)
+# Automatisera nedladdning av Trafikverkets NVDB länsdata, dvs en länsdatabas
+# nedladdat data skriver över tidigare nedladdning
 
-# För att komma förbi proxyn
-set_config(use_proxy(url = "http://mwg.ltdalarna.se", port = 9090))
-set_config(config(ssl_verifypeer = 0L))
+
+
+##################################################################################
+### Clean start
+##################################################################################
+
+rm(list = ls())
+gc()
+
+
+##################################################################################
+### Libraries etc
+##################################################################################
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse,
+               httr, jsonlite)
+
+
+# avoid scientific notation
+options(scipen=999)
+
+
+
+##################################################################################
+### Define paths and create data directories (if not already exist)
+##################################################################################
+
+# create directories
+dir.create(paste0(getwd(), "/output/"))
+dir.create(paste0(getwd(), "/shapefile"))
+
+# define paths
+folder_github = "https://github.com/bjornsh/gis_data/raw/main/" 
+
+# Read keys 
+api_fil = read_file("Z:/api")
+UserName = gsub('^.*lastkajen_id: \\s*|\\s*\r.*$', "", api_fil)
+Password = gsub('^.*lastkajen_key: \\s*|\\s*\r.*$', "", api_fil)
+
+
+
+
+##################################################################################
+### Define variables
+##################################################################################
+
+# Vilket län 
+lan = "Uppsala län"
 
 # inställningar 
-sparafilmapp <- "G:/Samhällsanalys/GIS/NVDB/"
+sparafilmapp <- paste0(getwd(), "/shapefile/")
 tabortzipfil <- TRUE                  # ta bort zipfilen när alla filer är uppackade
 
-path <- "https://lastkajen.trafikverket.se/api/Identity/Login"
 
-inlogg <- POST(url = path, body = list(UserName = Sys.getenv("lastkajen_user"),
-                                       Password = Sys.getenv("lastkajen_pwd")), encode = "json")
+##################################################################################
+### Load data
+##################################################################################
+
+path <- "https://lastkajen.trafikverket.se/api/Identity/Login"
+query <- list(UserName = UserName,
+              Password = Password)
+
+inlogg <- POST(url = path, body = query, encode = "json")
 
 if (inlogg$status_code == 200){
   
@@ -27,15 +80,18 @@ if (inlogg$status_code == 200){
   path_mappar <- "https://lastkajen.trafikverket.se/api/DataPackage/GetDataPackages"
   mappar <- GET(url = path_mappar)
   resp_mappar <- fromJSON(content(mappar, as = "text"), flatten = TRUE)
-  transp_plan_id <- as.character(resp_mappar %>% filter(str_detect(name, "transportplanering")) %>% 
-    select(id))
+  transp_plan_id <- as.character(resp_mappar %>% filter(str_detect(sourceFolder, "Länsfiler NVDB-data") &
+                                                          str_detect(sourceFolder, lan)) %>%  
+                                   select(id))
   
   # ==== hitta filer i vald mapp (vägnät för transportplanering)
   path_fillista <- "https://lastkajen.trafikverket.se/api/DataPackage/GetDataPackageFiles"
   fillista <- GET(url = paste0(path_fillista, "/", transp_plan_id))
   resp_fillista <- fromJSON(content(fillista, as = "text"), flatten = TRUE)
-  transp_plan_namn <- resp_fillista[grepl("zip", resp_fillista$name) & grepl("Geopackage", resp_fillista$name),"name"]
-  transp_plan_url <- resp_fillista[grepl("zip", resp_fillista$name) & grepl("Geopackage", resp_fillista$name),"links"]
+  transp_plan_namn <- resp_fillista[grepl("zip", resp_fillista$name) & 
+                                      grepl("geopackage", tolower(resp_fillista$name)),"name"] # tolower (stavning skiljer sig mellan olika dataprodukter)
+  transp_plan_url <- resp_fillista[grepl("zip", resp_fillista$name) & 
+                                     grepl("geopackage", tolower(resp_fillista$name)),"links"]
   url_token <- transp_plan_url[[1]]$href[[2]]
   
   # här hämtar vi en token för att kunna ladda ner filen vi vill ha - den är giltig i 1 min
@@ -47,14 +103,10 @@ if (inlogg$status_code == 200){
   nedl_fil_full <- paste0(sparafilmapp, transp_plan_namn)
   GET(url = nedl_fil_path, write_disk(nedl_fil_full, overwrite = TRUE), progress())
   
-  # ladda upp filen(filerna) i zip-filen, gör bakcup av filer med samma namn i samma mapp om sådana finns
-  filer_i_zipfil <- unzip(nedl_fil_full, list = TRUE)$Name
-  for (zip_filnamn in 1:length(filer_i_zipfil)){
-    sparafil_en_backup_nvdb(paste0(sparafilmapp, filer_i_zipfil[[zip_filnamn]]))
-  }
-  zipmapp <- substr(sparafilmapp, 1, nchar(sparafilmapp)-1)    # skapa mapp-variabel för zipuppackning
-  unzip(nedl_fil_full, exdir = zipmapp)                        # packa upp zip-fil
-  if (tabortzipfil) file.remove(nedl_fil_full)                                   # ta bort zip fil
+  # packa upp zip-fil och spara filer
+  unzip(nedl_fil_full, exdir = sparafilmapp)
+
+  # ta bort zip fil
+  if (tabortzipfil) file.remove(nedl_fil_full)                 
   
-  rm(inlogg)               # ta bort inlogg som innehåller anvnamn och lösenord 
 }
